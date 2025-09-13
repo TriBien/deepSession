fa0e63e4bccc28efe9dfcdb5723f4bac52e7515f
tri <bienthanhtri@gmail.com>
2025-09-13 15:44:51 +0700

    Fix error

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@ const ASSETS = [
   "/icons/favicon-16x16.png",
   "/icons/favicon-32x32.png",
   "/icons/apple-touch-icon.png",
-  "icons/android-chrome-192x192.png",
+  "/icons/android-chrome-192x192.png",
   "/icons/android-chrome-512x512.png",
   "/site.webmanifest",
 ];
@@ -18,7 +18,16 @@ const ASSETS = [
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+      return Promise.all(
+        ASSETS.map((url) =>
+          fetch(url)
+            .then((response) => {
+              if (!response.ok) throw new Error(`Request failed for ${url}`);
+              return cache.put(url, response);
+            })
+            .catch((err) => console.warn("Skipping:", url, err))
+        )
+      );
     })
   );
   self.skipWaiting();
